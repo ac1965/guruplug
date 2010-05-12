279e466d50b5854c42ad1903045d0ecac6452bf3
YAMASHITA Takao <tjy1965@gmail.com>
2010-05-12 15:16:30 +0900

    zzz

--- gentoo/script/chroot.sh
+++ gentoo/script/chroot.sh
@@ -7,6 +7,7 @@ CHOST=armv5tel-softfloat-linux-gnueabi
 SYSROOT="/usr/${CHOST}"
 PKGCONFIG="${SYSROOT}"/usr/lib/pkgconfig
 
+
 case "$1" in
 --install|-install|-i) # make stage3 (cross-compiled)
 	ROOT="/root/${CHOST}-rfs"
@@ -16,20 +17,22 @@ case "$1" in
 	;;
 esac
 
+export ROOT SYSROOT PKGCONFIG
+
 [ -d ${SYSROOT} ] || install -d ${SYSROOT}
 
-ROOT=${ROOT} SYSROOT=${SYSROOT} PKGCONFIG=${PKGCONFIG} armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  --emptytree sys-apps/acl
-ROOT=${ROOT} SYSROOT=${SYSROOT} PKGCONFIG=${PKGCONFIG} armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  --emptytree sys-libs/gdbm
-ROOT=${ROOT} SYSROOT=${SYSROOT} PKGCONFIG=${PKGCONFIG} armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  --emptytree dev-db/sqlite
-ROOT=${ROOT} SYSROOT=${SYSROOT} PKGCONFIG=${PKGCONFIG} armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  sys-libs/zlib
-ROOT=${ROOT} SYSROOT=${SYSROOT} PKGCONFIG=${PKGCONFIG} armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  dev-libs/expat
-ROOT=${ROOT} SYSROOT=${SYSROOT} PKGCONFIG=${PKGCONFIG} armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  app-arch/xz-utils
-ROOT=${ROOT} SYSROOT=${SYSROOT} PKGCONFIG=${PKGCONFIG} armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  dev-libs/libffi
-ROOT=${ROOT} SYSROOT=${SYSROOT} PKGCONFIG=${PKGCONFIG} armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  sys-apps/sandbox
-ROOT=${ROOT} SYSROOT=${SYSROOT} PKGCONFIG=${PKGCONFIG} armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  dev-libs/gmp
-ROOT=${ROOT} SYSROOT=${SYSROOT} cross-fix-root
-ROOT=${ROOT} SYSROOT=${SYSROOT} PKGCONFIG=${PKGCONFIG} armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  dev-libs/mpfr
-ROOT=${ROOT} SYSROOT=${SYSROOT} PKGCONFIG=${PKGCONFIG} armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  dev-lang/perl
-ROOT=${ROOT} SYSROOT=${SYSROOT} PKGCONFIG=${PKGCONFIG} armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  dev-lang/python
-ROOT=${ROOT} SYSROOT=${SYSROOT} cross-fix-root
-ROOT=${ROOT} SYSROOT=${SYSROOT} PKGCONFIG=${PKGCONFIG} armv5tel-softfloat-linux-gnueabi-emerge ${eopts} --update system
+armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  --emptytree sys-apps/acl
+armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  --emptytree sys-libs/gdbm
+armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  --emptytree dev-db/sqlite
+armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  sys-libs/zlib
+armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  dev-libs/expat
+armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  app-arch/xz-utils
+armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  dev-libs/libffi
+armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  sys-apps/sandbox
+armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  dev-libs/gmp
+cross-fix-root
+armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  dev-libs/mpfr
+armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  dev-lang/perl
+armv5tel-softfloat-linux-gnueabi-emerge ${eopts}  dev-lang/python
+cross-fix-root
+armv5tel-softfloat-linux-gnueabi-emerge ${eopts} --update system
