da9e9e8fbacf0cabeeb6b1187cc7d28c40817880
YAMASHITA Takao <tjy1965@gmail.com>
2010-05-12 15:11:11 +0900

    renew

---
+++ gentoo/script/q/q-chroot.sh
@@ -0,0 +1,39 @@
+#! /bin/sh
+
+rfs=/root/armv5tel-softfloat-linux-gnueabi-rfs
+files=/root/q # qemu-wrapper, this script ..
+
+[ -d /proc/sys/fs/binfmt_misc ] || modprobe binfmt_misc
+[ -f /proc/sys/fs/binfmt_misc/register ] || (
+    mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
+    echo ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/qemu-wrapper:' > /proc/sys/fs/binfmt_misc/register
+)
+
+[ -d ${rfs}/proc ] || install -d ${rfs}/proc
+[ -d ${rfs}/sys ] || install -d ${rfs}/sys
+[ -d ${rfs}/dev ] || install -d ${rfs}/dev
+[ -d ${rfs}/dev/pts ] || install -d ${rfs}/dev/pts
+[ -d ${rfs}/root ] || install -d ${rfs}/root
+[ -f ${rfs}/qemu-wrapper ] || cp ${files}/qemu-wrapper ${rfs}
+
+mount --bind /proc ${rfs}/proc
+mount --bind /sys ${rfs}/sys
+
+cp -L /etc/resolv.conf ${rfs}/etc
+chroot ${rfs} /bin/busybox mdev -s
+ps1='`uname -m`'
+cat <<EOF > ${rfs}/root/dot.bashrc
+gcc-config 1
+env-update
+source /etc/profile
+export PS1="($ps1:chroot:\W) "
+alias emerge='emerge --jobs=4'
+EOF
+chroot ${rfs} /bin/bash --rcfile /root/dot.bashrc
+
+rm -f ${rfs}/etc/resolv.conf
+rm -f ${rfs}/root/.bashrc_history
+
+umount ${rfs}/sys
+umount ${rfs}/proc
+umount /proc/sys/fs/binfmt_misc
--- gentoo/script/vt.sh
+++ gentoo/script/vt.sh
@@ -2,7 +2,7 @@
 
 arch='arm'
 base='10.0'
-rev='last'
+rev='b'
 
 abort () {
 	echo $1
@@ -25,36 +25,34 @@ esac
 r=/root/cross
 
 distrib=${r}/gentoo-dist
-portage=${r}/portage
-distfiles=${r}/distfiles
+portage=${r}/${rev}/portage
+distfiles=${r}/${rev}/distfiles
 lib_layman=${distrib}/lib_layman
 local_overlay=${distrib}/local_overlay
 host_pkgs=${r}/${rev}/host
 target_tmp=${r}/${rev}/target
 target_pkgs=${r}/${rev}/binpkgs
-native_tmp=${r}/${rev}/native
-native_pkgs=${r}/${rev}/native_binpkgs
+native_pkgs=${r}/${rev}/target_native
 stage=${r}/${rev}/rfs
 
 host_portage=${distrib}/host_etc_portage
 target_portage=${distrib}/target_etc_portage
 
 rfs=${distrib}/gentoo
-[ -d ${rfs}/usr/${target} ] || abort "You must run crossdev."
 
 [ x"${arg1}" = x"init" ] && (
 	[ -d ${distrib}/${rev} ] || install -d ${distrib}/${rev}
 	[ -d ${r}/${rev} ] || install -d ${r}/${rev}
 	[ -d ${distrib}/${rev}/portage ] || install -d ${distrib}/${rev}/portage
 	[ -d ${distrib}/${rev}/binpkgs ] || install -d ${distrib}/${rev}/binpkgs
-	[ -d ${distrib}/${rev}/native_binpkgs ] || install -d ${distrib}/${rev}/native_binpkgs
-	[ -d ${distrib}/${rev}/rfs ] || install -d ${distrib}/${rev}/rfs
+	[ -d ${distrib}/${rev}/target_native ] || install -d ${distrib}/${rev}/target_native
+	[ -L ${portage} ] || ln -s ${distrib}/${rev}/portage ${portage}
 	[ -L ${target_pkgs} ] || ln -s ${distrib}/${rev}/binpkgs ${target_pkgs}
-	[ -L ${native_pkgs} ] || ln -s ${distrib}/${rev}/native_binpkgs ${native_pkgs}
+	[ -L ${native_pkgs} ] || ln -s ${distrib}/${rev}/target_native ${native_pkgs}
 )
 
 [ -d ${rfs} ] || abort "${rfs} not found."
-[ -d ${portage} ] || abort "${portage} not found. you must run \``basename $0` init\`"
+[ -L ${portage} ] || abort "${portage} not found. you must run \``basename $0` init\`"
 
 [ -d ${distfiles} ] || install -d ${distfiles}
 [ -d ${host_portage} ] || install -d ${host_portage}
@@ -64,8 +62,6 @@ rfs=${distrib}/gentoo
 [ -d ${host_pkgs} ] || install -d ${host_pkgs}
 [ -d ${target_tmp} ] || install -d ${target_tmp}
 [ -d ${target_tmp}/binpkgs ] || install -d ${target_tmp}/binpkgs
-[ -d ${native_tmp} ] || install -d ${native_tmp}
-[ -d ${native_tmp}/binpkgs ] || install -d ${native_tmp}/binpkgs
 [ -d ${stage} ] || install -d ${stage}
 
 [ -d ${rfs}/dev ] || install -d ${rfs}/dev
@@ -79,13 +75,10 @@ rfs=${distrib}/gentoo
 [ -d ${rfs}/usr/${target}/etc/portage ] || install -d ${rfs}/usr/${target}/etc/portage
 [ -d ${rfs}/var/tmp/native ] || install -d ${rfs}/var/tmp/native
 [ -d ${rfs}/var/tmp/cross ] || install -d ${rfs}/var/tmp/cross
+[ -d ${rfs}/usr/${target}/var/tmp/native ] || install -d ${rfs}/usr/${target}/var/tmp/native
 [ -d ${rfs}/var/lib/layman ] || install -d ${rfs}/var/lib/layman
 [ -d ${rfs}/usr/local/overlay ] || install -d ${rfs}/usr/local/overlay
-[ -d ${rfs}/root/${target}-rfs ] || install -d ${rfs}/root/${target}-rfs
-[ -d ${rfs}/root/${target}-rfs/etc/portage ] || install -d ${rfs}/root/${target}-rfs/etc/portage
-[ -d ${rfs}/root/${target}-rfs/usr/portage ] || install -d ${rfs}/root/${target}-rfs/usr/portage
-[ -d ${rfs}/root/${target}-rfs/var/tmp/native ] || install -d ${rfs}/root/${target}-rfs/var/tmp/native
-[ -d ${rfs}/root/${target}-rfs/var/tmp/distfiles ] || install -d ${rfs}/root/${target}-rfs/var/tmp/distfiles
+[ -d ${rfs}/root/stage ] || install -d ${rfs}/root/stage
 
 cd ${distrib}
 mount -o bind /dev ${rfs}/dev
@@ -100,14 +93,10 @@ mount -o bind ${target_portage} ${rfs}/usr/${target}/etc/portage
 mount -o bind ${host_pkgs} ${rfs}/var/tmp/native
 mount -o bind ${target_tmp} ${rfs}/var/tmp/cross
 mount -o bind ${target_pkgs} ${rfs}/var/tmp/cross/binpkgs
+mount -o bind ${native_pkgs} ${rfs}/usr/${target}/var/tmp/native
 mount -o bind ${lib_layman} ${rfs}/var/lib/layman
 mount -o bind ${local_overlay} ${rfs}/usr/local/overlay
-mount -o bind ${stage} ${rfs}/root/${target}-rfs
-mount -o bind ${target_portage} ${rfs}/root/${target}-rfs/etc/portage
-mount -o bind ${portage} ${rfs}/root/${target}-rfs/usr/portage
-mount -o bind ${native_tmp} ${rfs}/root/${target}-rfs/var/tmp/native
-mount -o bind ${native_pkgs} ${rfs}/root/${target}-rfs/var/tmp/native/binpkgs
-mount -o bind ${distfiles} ${rfs}/root/${target}-rfs/var/tmp/distfiles
+mount -o bind ${stage} ${rfs}/root/stage
 
 [ x"${arg1}" = x"proxy_use" ] && (
 echo proxy using.
@@ -115,11 +104,7 @@ test -d ${rfs}/.subversion
 cp ${distrib}/files/proxyuse ${rfs}/root/.proxyuse
 cp ${distrib}/files/servers ${rfs}/root
 )
-
-ps1='`uname -m`'
-cat <<EOF > ${rfs}/root/dot.bashrc
-env-update
-source /etc/profile
+cat <<EOF > gentoo/root/dot.bashrc
 [ -f /root/.proxyuse ] && (
 	svn help >/dev/null 2>&1
 	test -f /root/.subversion || mv /root/servers /root/.subversion
@@ -128,11 +113,10 @@ source /etc/profile
 [ -L /usr/${target}/etc/make.conf ] || ln -s portage/make.conf-${arch} /usr/${target}/etc/make.conf
 [ -L /usr/${target}/etc/make.profile ] || ln -s ../usr/portage/profiles/default/linux/${arch}/${base} /usr/${target}/etc/make.profile
 export STAGEDIR="/usr/${target}"
-export PS1="(${ps1}:chroot:\W) "
+export PS1="(`uname -m`:chroot:\W) "
 alias pkg-config='cross-pkg-config'
 EOF
 #export CROSS_COMPILE="${target}-"
-cp ${distrib}/files/chroot.sh ${rfs}/root
 cp -L /etc/resolv.conf ${rfs}/etc/resolv.conf
 
 chroot ${rfs} /bin/bash --rcfile /root/dot.bashrc
@@ -144,16 +128,12 @@ rm -fr ${rfs}/root/.bash_history
 rm -fr ${rfs}/root/.subversion
 rm -fr ${rfs}/root/.script
 rm -fr ${rfs}/root/.viminfo
-rm -fr ${rfs}/root/chroot.sh
 rm -fr ${rfs}/etc/resolv.conf
 
-umount ${rfs}/root/${target}-rfs/var/tmp/native/binpkgs
-umount ${rfs}/root/${target}-rfs/var/tmp/native
-umount ${rfs}/root/${target}-rfs/usr/portage
-umount ${rfs}/root/${target}-rfs/etc/portage
-umount ${rfs}/root/${target}-rfs
+umount ${rfs}/root/stage
 umount ${rfs}/usr/local/overlay
 umount ${rfs}/var/lib/layman
+umount ${rfs}/usr/${target}/var/tmp/native
 umount ${rfs}/var/tmp/cross/binpkgs
 umount ${rfs}/var/tmp/cross
 umount ${rfs}/var/tmp/native
